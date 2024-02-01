cfa451b89afd6a416cd17af4c1286b170b0fcb34
s12171934 <ssong99999@naver.com>
2024-02-01 15:43:24 +0900

    json 형식의 데이터 삽입시 기능들이 구현하도록 변경

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,22 +1,55 @@
-function completeToDoList(elem){
-    if(elem.className == ""){
-        elem.className = "complete";
-    } else{
-        elem.className = "";
+let num = 3;
+
+let data = [
+    {
+        "id" : 1,
+        "todo" : "123",
+        "stat" : ""
+    },
+    {
+        "id" : 2,
+        "todo" : "456",
+        "stat" : ""
     }
-    
+];
+
+function completeToDoList(elem){
+    console.log(elem)
+    if(elem.className == ""){
+        data.filter(list => list.id == elem.id)[0].stat = "complete";
+    } else{
+        data.filter(list => list.id == elem.id)[0].stat = "";
+    }
 }
 
 function deleteToDoList(elem){
-    elem.remove();
+    let idx = data.findIndex(list => list.id == elem.id);
+    console.log(idx);
+    data.splice(idx,idx + 1);;
 }
 
 function addToDoList(elem){
     let list = elem.firstElementChild.value;
+    if(list === "") return;
     elem.firstElementChild.value = "";
-    let li = document.createElement("li");
-    li.innerHTML = list;
-    document.querySelector(".todos").append(li);
+    
+    data.push({
+        "id" : num++,
+        "todo" : list,
+        "stat" : ""
+    })
+    
+}
+
+function showToDoList(){
+    document.querySelector(".todos").innerHTML = "";
+    for(list of data){
+        let li = document.createElement("li");
+        li.id = list.id;
+        li.className = list.stat;
+        li.textContent = list.todo;
+        document.querySelector(".todos").append(li);
+    }
     
 }
 
@@ -24,7 +57,8 @@ document.querySelector("form").addEventListener('submit',(event) => {
     event.preventDefault();
     console.log(event.target)
     addToDoList(event.target);
-})
+    showToDoList();
+});
 
 window.oncontextmenu = function(){
     return false;
@@ -33,10 +67,11 @@ window.oncontextmenu = function(){
 document.querySelector(".todos").addEventListener('mousedown', (event) =>{
     if((event.button === 2)||(event.which ===3)){
         deleteToDoList(event.target);
-        console.log(event.target);
+        console.log(event.target)
     } else{
-        console.log(event.target.className)
         completeToDoList(event.target);
-        console.log(event.target);
     }
-})
+    showToDoList();
+});
+
+showToDoList();
