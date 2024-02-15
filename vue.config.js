2946e9b63afe78024f519bb8aaa0f3990d5e7ea8
s12171934 <ssong99999@naver.com>
2024-02-15 15:54:24 +0900

    https://github.com/s12171934/To-Do-LIst를 API server으로 가지는 F.E.임 vue.js.로 작성 아직 session로그인기능이 추가되지 않아, localhost:8080에서 로그인 진행후에 작동 가능

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false,
 })
+
+module.exports = {
+  devServer: {
+    proxy: {
+      "/api": {	
+        target: "http://localhost:8080", // 프록시를 설정할 도메인
+        changeOrigin: true,
+      }, 
+    }
+  },
+  lintOnSave: false,
+}
+
